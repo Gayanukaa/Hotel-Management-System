f50c6b61f76ddfb172679eb651012616a665059c
Gayanuka Amarasuriya <gayanukaamarasuriya@gmail.com>
2023-08-02 00:33:24 +0530

    New room display criteria

--- Interfaces/Room_Details.py
+++ Interfaces/Room_Details.py
@@ -13,15 +13,52 @@ class RoomDetails:
         self.root.resizable(False, False)
         center(self.root,1000,600)
 
-        img = PhotoImage(file="Images/Backgrounds/Gradient_background_9.png")
+        img = PhotoImage(file="Images/Backgrounds/Gradient_background_10.png")
         Label(self.root, image=img).pack()
 
         #self.root.iconbitmap("Images/hnet.com-image.ico")  #For MacOs
         #self.root.iconphoto(False, PhotoImage(file = "Images/hnet.com-image.png")) #For Windows
         
+        Label(self.root, text="Room Details", font=("times new roman",30, "bold")).place(x=500,y=50, anchor="center") 
         
-       
+        buttonFrame = Frame(self.root)
+        buttonFrame.place(x=40, y=100, width=140, height=470)
+        
+        Button(buttonFrame,text="Single Room",relief=RAISED,borderwidth=3,font=("times new roman",15,"bold"),command=None).pack(padx = 10, pady= 17)
+        Button(buttonFrame,text="Double Room",relief=RAISED,borderwidth=3,font=("times new roman",15,"bold"),command=None).pack(padx = 10, pady= 17)
+        Button(buttonFrame,text="Triple Room",relief=RAISED,borderwidth=3,font=("times new roman",15,"bold")).pack(padx = 10, pady=17)
+        Button(buttonFrame,text="Quad Room",relief=RAISED,borderwidth=3,font=("times new roman",15,"bold"),command=None).pack(padx = 10, pady= 17)
+        Button(buttonFrame,text="Queen Room",relief=RAISED,borderwidth=3,font=("times new roman",15,"bold")).pack(padx = 10, pady= 17)
+        Button(buttonFrame,text="King Room",relief=RAISED,borderwidth=3,font=("times new roman",15,"bold"),command=None).pack(padx = 10, pady= 17)
+        Button(buttonFrame,text="Twin Room",relief=RAISED,borderwidth=3,font=("times new roman",15,"bold")).pack(padx = 10, pady= 17)
+
+        self.singleFrame = Frame(self.root,width=770, height=500)
+        self.singleFrame.pack(padx=210,pady=80)
+        #self.singleFrame.place(x=210, y=80)
+        
+        singleRoom = PhotoImage(file="Images/Rooms/Single_Room_1.png",height=400,width=600)
+        Label(self.singleFrame, image=singleRoom).place(x=0, y=0)
+        
+        #text description about single room
+        Label(self.singleFrame, text="Single Room", font=("times new roman",20, "bold")).place(x=540,y=20)
+        
+        
+        Label(self.singleFrame, text="Room Size: 20m2", font=("times new roman",15, "bold")).place(x=430,y=80)
+        Label(self.singleFrame, text="Bed Size: 1 Single Bed", font=("times new roman",15, "bold")).place(x=430,y=120)
+        Label(self.singleFrame, text="Max People: 1", font=("times new roman",15, "bold")).place(x=430,y=160)
+        Label(self.singleFrame, text="Room View: City View", font=("times new roman",15, "bold")).place(x=430,y=200)
+        Label(self.singleFrame, text="Room Price: $100", font=("times new roman",15, "bold")).place(x=430,y=240)
+        Label(self.singleFrame, text="Room Description: ", font=("times new roman",15, "bold")).place(x=430,y=280)
+        Label(self.singleFrame, text="This room is a single room with a single bed.\nIt has a city view and is 20m2 in size.", font=("times new roman",15, "bold")).place(x=440,y=320)
+        
+        Button(self.singleFrame,text="Book Now",relief=RAISED,borderwidth=3,font=("times new roman",15,"bold"),command=None).place(x=350,y=450)
+        
+        #self.singleFrame.pack_forget()      
+        
+        
         self.root.mainloop()
+        
+        
         
     """
         self.currentPage = 1
